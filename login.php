e0db01830b7d35d5dff03ecc38b19f26dc843ab0
Reihandi <Reihandiputra@gmail.com>
2023-05-27 19:46:39 +0700

    Backend Login + button register

--- login.php
+++ login.php
@@ -1,3 +1,61 @@
+<?php
+session_start();
+include('server/connection.php');
+
+if (isset($_SESSION['logged_in'])) {
+    header('location: index.php');
+    exit;
+}
+
+if (isset($_POST['register_btn'])) {
+    header('location: register.html');
+}
+
+
+if (isset($_POST['login_btn'])) {
+    $email = $_POST['user_email'];
+    $password = ($_POST['user_password']);
+
+    $query = "SELECT user_id, user_email, user_password, user_name, user_saldo,
+        user_photo FROM user WHERE user_email = ? 
+        AND user_password = ? LIMIT 1";
+
+    $stmt_login = $conn->prepare($query);
+    $stmt_login->bind_param('ss', $email, $password);
+
+    if ($stmt_login->execute()) {
+        $stmt_login->bind_result(
+            $user_id,
+            $user_name,
+            $user_email,
+            $user_password,
+            $user_saldo,
+            $user_photo
+        );
+        $stmt_login->store_result();
+
+        if ($stmt_login->num_rows() == 1) {
+            $stmt_login->fetch();
+
+            $_SESSION['user_id'] = $user_id;
+            $_SESSION['user_name'] = $user_name;
+            $_SESSION['user_email'] = $user_email;
+            $_SESSION['user_password'] = $user_password;
+            $_SESSION['user_saldo'] = $user_phone;
+            $_SESSION['user_photo'] = $user_photo;
+            $_SESSION['logged_in'] = true;
+
+
+            header('location: index.php?message=Logged in successfully');
+        } else {
+            header('location: login.php?error=Could not verify your account!');
+        }
+    } else {
+        header('location: login.php?error=Something went wrong!');
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,15 +81,23 @@
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title ms-3">Login</h5>
-                        <form>
+                        <form action="login.php" method="post">
                             <div class="mb-3">
-                                <input type="email" class="form-control rounded-4" id="exampleInputEmail1">
+                                <input type="email" class="form-control rounded-4" name="user_email">
                             </div>
                             <div class="mb-3">
-                                <input type="password" class="form-control rounded-4" id="exampleInputPassword1">
+                                <input type="password" class="form-control rounded-4" name="user_password">
                             </div>
                             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                <button class="btn me-md-2 rounded-4" type="button">Login</button>
+                                <button class="btn me-md-2 rounded-4" type="submit" id="login-btn" name="login_btn">LOGIN</button>
+                            </div>
+                            <div class="daftar">
+                                <a href="register.php"> Register</a>
+                            </div>
+                            <div class="error">
+                                <?php if (isset($_GET['error'])) {
+                                    echo $_GET['error'];
+                                } ?>
                             </div>
                         </form>
                     </div>
