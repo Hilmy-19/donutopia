11d439185fc25f2393e487dbc6077fc44894b5f2
Reihandi <Reihandiputra@gmail.com>
2023-05-28 16:53:02 +0700

    Membuat Frontend profile

---
+++ profile.php
@@ -0,0 +1,25 @@
+<?php
+include 'layouts/header.php';
+?>
+
+<link rel="stylesheet" href="assets/css/profile.css">
+
+
+<div class="container1">
+    <div class="card">
+        <div class="profile">
+            <div class="profile-picture">
+                <img src="assets/image/process3.jpg" alt="Profil Picture">
+            </div>
+            <div class="profile-info">
+                <h2>Nama Pengguna</h2>
+                <p>Email: user@example.com</p>
+                <p>Saldo: $100</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include 'layouts/footer.php'
+?>
